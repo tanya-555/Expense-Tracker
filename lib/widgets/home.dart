b894f98c441b087dc1ad4a59c37bbd1ac10d494f
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-01 16:59:32 +0530

    use constraint to set height and width of container

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -46,6 +46,8 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     return new MaterialApp(
       title: "Personal Expense Tracker",
       theme: ThemeData(
@@ -57,49 +59,50 @@ class _HomeState extends State<Home> {
         ),
         body: LayoutBuilder(builder: (ctx, constraint) {
           return Container(
-              child: _expensesList.isEmpty
-                  ? Column(
-                      children: <Widget>[
-                        Container(
+            child: _expensesList.isEmpty
+                ? Column(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset(
+                          'assets/images/attachment.jpg',
+                          width: constraint.maxWidth * 0.8,
+                          height: constraint.maxHeight * 0.6,
                           alignment: Alignment.center,
-                          child: Image.asset(
-                            'assets/images/attachment.jpg',
-                            width: constraint.maxWidth * 0.8,
-                            height: constraint.maxHeight * 0.6,
-                            alignment: Alignment.center,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          "No Item to display!",
+                          style: TextStyle(
+                            color: Colors.cyan,
+                            fontSize: 15.0,
                           ),
                         ),
-                        Container(
-                          padding: EdgeInsets.all(20),
-                          child: Text(
-                            "No Item to display!",
-                            style: TextStyle(
-                              color: Colors.cyan,
-                              fontSize: 15.0,
-                            ),
-                          ),
+                      ),
+                    ],
+                  )
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                          width: constraint.maxWidth * 1,
+                          height: constraint.maxHeight * 0.4,
+                          child: Container(
+                            child: PieChartRep(chart),
+                          )),
+                      Container(
+                        height: constraint.maxHeight * 0.6,
+                        width: constraint.maxWidth * 1,
+                        child: ExpenseList(
+                          expensesList: _expensesList,
+                          selectHandler: removeItem,
                         ),
-                      ],
-                    )
-                  : Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                            width: 500,
-                            height: 200,
-                            child: Container(
-                              child: PieChartRep(chart),
-                            )),
-                        Container(
-                          height: 300,
-                          width: 500,
-                          child: ExpenseList(
-                            expensesList: _expensesList,
-                            selectHandler: removeItem,
-                          ),
-                        ),
-                      ],
-                    ));
+                      ),
+                    ],
+                  ),
+          );
         }),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
