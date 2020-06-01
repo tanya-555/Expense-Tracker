92d79fe8929e0fa7a2b9a41ee1c04b595dde710f
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-01 16:40:46 +0530

    use layout builder for displaying placeholder image in landscape mode

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -55,50 +55,52 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           title: Text("Personal Expense Tracker"),
         ),
-        body: Container(
-            child: _expensesList.isEmpty
-                ? Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(10.0),
-                        margin: EdgeInsets.only(top: 100),
-                        child: Image.asset(
-                          'assets/images/attachment.jpg',
-                          width: 500,
-                          height: 300,
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          "No Item to display!",
-                          style: TextStyle(
-                            color: Colors.cyan,
-                            fontSize: 15.0,
+        body: LayoutBuilder(builder: (ctx, constraint) {
+          return Container(
+              child: _expensesList.isEmpty
+                  ? Column(
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/images/attachment.jpg',
+                            width: constraint.maxWidth * 0.8,
+                            height: constraint.maxHeight * 0.6,
+                            alignment: Alignment.center,
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                          width: 500,
-                          height: 200,
-                          child: Container(
-                            child: PieChartRep(chart),
-                          )),
-                      Container(
-                        height: 300,
-                        width: 500,
-                        child: ExpenseList(
-                          expensesList: _expensesList,
-                          selectHandler: removeItem,
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            "No Item to display!",
+                            style: TextStyle(
+                              color: Colors.cyan,
+                              fontSize: 15.0,
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
-                  )),
+                      ],
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            width: 500,
+                            height: 200,
+                            child: Container(
+                              child: PieChartRep(chart),
+                            )),
+                        Container(
+                          height: 300,
+                          width: 500,
+                          child: ExpenseList(
+                            expensesList: _expensesList,
+                            selectHandler: removeItem,
+                          ),
+                        ),
+                      ],
+                    ));
+        }),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
